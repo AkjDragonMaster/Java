c290d6a04c9cfd2f64b5629feca69e20680f3b05
AkjDragonMaster <76256480+AkjDragonMaster@users.noreply.github.com>
2022-01-30 00:34:40 +0530

    Create Computer_guesses_number.java

---
+++ Computer_guesses_number.java
@@ -0,0 +1,23 @@
+public class Computer_guesses_number
+{
+	public static void main(String[] args) {
+	    java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Hello! \nDo you want to play a game?");
+		System.out.println("1 for Yes \n2 for No");
+		System.out.print("Answer: ");
+		int ans = sc.nextInt();
+		if(ans == 1){
+		    System.out.println("Ok! So, first choose a number in your mind");
+		    System.out.println("Now, divide it by 5.");
+		    System.out.println("Next! multiply it by 4.");
+		    System.out.println("Now, again, multiply it by 2");
+		    System.out.print("Good! Now give me the number you got - ");
+		    int num = sc.nextInt();
+		    double ogNum = num * 0.625;
+		    System.out.println("Your original number is "+ogNum);
+		}else{
+		    System.out.println("Oh! Sorry to disturb you.");
+		}
+		
+	}
+}
