0a6eda7adfe7be79fcaee1297d6231a62b825094
AkjDragonMaster <76256480+AkjDragonMaster@users.noreply.github.com>
2022-01-30 14:02:39 +0530

    Update Computer_guesses_number.java

--- Computer_guesses_number.java
+++ Computer_guesses_number.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class Computer_guesses_number
 {
 	public static void main(String[] args) {
@@ -7,14 +8,18 @@ public class Computer_guesses_number
 		System.out.print("Answer: ");
 		int ans = sc.nextInt();
 		if(ans == 1){
-		    System.out.println("Ok! So, first choose a number in your mind");
+		    System.out.println("Ok! So, first choose an integer in your mind");
 		    System.out.println("Now, divide it by 5.");
 		    System.out.println("Next! multiply it by 4.");
 		    System.out.println("Now, again, multiply it by 2");
 		    System.out.print("Good! Now give me the number you got - ");
-		    int num = sc.nextInt();
+		    double num = sc.nextFloat();
 		    double ogNum = num * 0.625;
-		    System.out.println("Your original number is "+ogNum);
+		    if(num%5 != 0 || num%2 != 0){
+		       System.out.println("Your original number is "+Math.round(ogNum)); 
+		    }else{
+		       System.out.println("Your original number is "+ogNum); 
+		    }
 		}else{
 		    System.out.println("Oh! Sorry to disturb you.");
 		}
