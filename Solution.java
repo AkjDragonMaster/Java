43161ff97db252a82302d821d7e37712ecf52d5d
AkjDragonMaster <76256480+AkjDragonMaster@users.noreply.github.com>
2022-03-19 12:24:21 +0530

    Solution

---
+++ Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        String s = sc.nextLine();
+        System.out.print(i+"\n"+d+"\n"+s+"\n");
+    }
+}
